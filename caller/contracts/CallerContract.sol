96c828478079c646f1a7b67ba646adea3fc7fa00
Tai <4382091+taisukemino@users.noreply.github.com>
2020-11-25 10:18:23 +0900

    instantiate the oracle contract

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,9 +1,15 @@
 pragma solidity 0.6.0;
+import '../../oracle/contracts/IETHPriceOracle.sol';
 
 contract CallerContract {
-    address private oracleAddress;
+    IETHPriceOracle private ETHPriceOracleInstance;
+    address private ETHPriceOracleAddress;
+    event newOracleAddressRegistered(address oracleAddress);
 
     setOracleInstanceAddress(address _oracleInstanceAddress) public {
-      oracleAddress = _oracleInstanceAddress;
+      ETHPriceOracleAddress = _ETHPriceOracleInstanceAddress;
+      ETHPriceOracleInstance = IETHPriceOracle(ETHPriceOracleAddress);
+      
+      emit newOracleAddressRegistered(ETHPriceOracleAddress);
     }
 }
