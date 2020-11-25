de32b39f437119e6fbef45dbceb6777fe419d633
Tai <4382091+taisukemino@users.noreply.github.com>
2020-11-25 10:18:55 +0900

    add the onlyOwner modifier to the function that sets an oracle address

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -1,12 +1,13 @@
 pragma solidity 0.6.0;
 import '../../oracle/contracts/IETHPriceOracle.sol';
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract CallerContract {
+contract CallerContract is Ownable {
     IETHPriceOracle private ETHPriceOracleInstance;
     address private ETHPriceOracleAddress;
     event newOracleAddressRegistered(address oracleAddress);
 
-    setOracleInstanceAddress(address _oracleInstanceAddress) public {
+    setOracleInstanceAddress(address _ETHPriceOracleInstanceAddress) public onlyOwner {
       ETHPriceOracleAddress = _ETHPriceOracleInstanceAddress;
       ETHPriceOracleInstance = IETHPriceOracle(ETHPriceOracleAddress);
       
