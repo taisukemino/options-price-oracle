3687ff685cb77ba39fb7e9e7497058bf1a394d69
Tai <4382091+taisukemino@users.noreply.github.com>
2020-11-25 18:55:04 +0900

    set the latest eth price

--- oracle/contracts/ETHPriceOracle.sol
+++ oracle/contracts/ETHPriceOracle.sol
@@ -1,11 +1,15 @@
 pragma solidity 0.6.0;
+import "../../contracts/ICaller.sol";
 import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
 interface ETHPriceOracle is Ownable {
+    ICaller private ICallerInstance;
     uint256 private nonce = 0;
     uint256 private modulus = 1000;
     mapping(uint256 => bool) pendingRequests;
     event GetLatestETHPrice(address callerAddress, uint id);
+    event SetLatestETHPrice(uint256 ethPrice, address callerAddress);
+
     function getLatestETHPrice() public returns (uint256){
         nonce++;
         uint256 id = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, nonce))) % modulus;
@@ -15,3 +19,13 @@ interface ETHPriceOracle is Ownable {
 
         return id;
     }
+
+    function setLatestETHPrice(uint256 _ethPrice, address callerAddress, uint256 _id) public onlyOwner {
+        require(pendingRequests[_id] == true, "This request is not in my pending list.");
+        delete pendingRequests[_id];
+        ICallerInstance = ICaller(callerAddress);
+        ICallerInstance.callback(_ethPrice, _id);
+      
+        emit SetLatestETHPrice(_ethPrice, callerAddress); 
+    }
+}
